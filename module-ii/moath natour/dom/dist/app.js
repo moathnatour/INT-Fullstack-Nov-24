d23fb4b2fb41b53191a3e632329da201f0f66de3
moathnatour <moathnatour6@gmail.com>
2025-02-02 17:02:38 +0200

    Merge https://github.com/dolevoper/INT-Fullstack-Nov-24

---
+++ module-ii/moath natour/dom/dist/app.js
@@ -0,0 +1,65 @@
+var buttons = Array.from(document.querySelectorAll("button"));
+var operators = Array.from(document.querySelectorAll(".operator"));
+var output = document.querySelector(".display");
+var number = "";
+var operand;
+var result;
+var lastOperator;
+buttons.forEach(function (button) { return button.addEventListener("click", function () {
+    if (!output) {
+        return;
+    }
+    if (!Number.isNaN(Number(button.textContent)) && button.textContent !== null) {
+        number += button.textContent;
+        output.innerText = number;
+    }
+    if (button.textContent === "C") {
+        output.innerText = "0";
+        number = "";
+        result = undefined;
+        operand = undefined;
+    }
+    if (operators.find(function (operator) { return operator.textContent === button.textContent; })) {
+        if (operand) {
+            calculate(lastOperator);
+            output.innerText = String(result);
+        }
+        operand = Number(output.innerText);
+        lastOperator = button.textContent;
+        number = "";
+    }
+    if (button.textContent === "=") {
+        // if(result === undefined || operand === undefined){
+        //     return;
+        // }
+        calculate(lastOperator);
+        output.innerText = String(result);
+        operand = undefined;
+    }
+}); });
+function calculate(input) {
+    if (result === undefined) {
+        result = operand;
+    }
+    console.log(operand);
+    console.log(input);
+    switch (input) {
+        case "+":
+            result = result + Number(number);
+            console.log(result);
+            return;
+        case "-":
+            result = result - Number(number);
+            console.log(result);
+            return;
+        case "/":
+            result = result / Number(number);
+            console.log(result);
+            return;
+        case "*":
+            result = result * Number(number);
+            console.log(result);
+            return;
+    }
+    console.log(result);
+}

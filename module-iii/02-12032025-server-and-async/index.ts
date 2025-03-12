8a3e6a78800954e51b164f2b7a378b009bd8f9a9
Omer Dolev <omerdolev90@gmail.com>
2025-03-12 18:59:52 +0200

    install tsx

--- module-iii/02-12032025-server-and-async/index.ts
+++ module-iii/02-12032025-server-and-async/index.ts
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 
-console.log(chalk.blue`hello`, add(5, 10));
+console.log(chalk.blue`hello`, chalk.yellowBright`world`, add(10, 10));
 
 function add(a: number, b: number) {
     return a + b;
